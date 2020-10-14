b6c2311aa2b2dc6d875cc25b32094e1569eb255e
Tanner Creque <tanner.creque@gmail.com>
2020-10-13 20:06:00 -0600

    Ruby work

---
+++ classes/variables.rb
@@ -0,0 +1,20 @@
+food = pizza
+is_delicious = true
+food = pasta
+num2 = 4
+num7 = 14
+num7/num2=quotient
+songs = ["10,000 Degrees", "I No Longer Fear The Razor Guarding My Heel"]
+shift "songs"
+
+
+puts "Enter some text"
+str1=gets.chomp
+
+count=0
+for i in 1..str1.length
+  if (str1 [i] != ' ')
+  end
+
+end
+puts "number of characters are #{count}}"
